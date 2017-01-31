efb13326bffcad0448aa44eba9d19ac47e3c3205
Mohamed Zaatar <mohamed.zaatar@readify.net>
2017-01-31 21:21:36 +0800

    Add Seq

--- WinningCards/Global.asax.cs
+++ WinningCards/Global.asax.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Web.Http;
+using Serilog;
+using System.Configuration;
 
 namespace WinningCards
 {
@@ -17,7 +15,15 @@ namespace WinningCards
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            var connectionString = ConfigurationManager.ConnectionStrings["seqmssql"].ConnectionString;
+            var tableName = "Logs";
+
+            var log = new LoggerConfiguration()
+                .WriteTo.MSSqlServer(connectionString, tableName)
+                .CreateLogger();
         }
     }
 }
